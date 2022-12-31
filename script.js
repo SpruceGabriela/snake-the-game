cf0a6dbc7f83ed5325eb7838f940163ffe950ec1
Lucas L. Lima <lucaslopes.developer@gmail.com>
2022-12-31 15:00:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); //....
-let box = 32;
+let box = 34;
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 snake[0] ={
     x: 8 * box,
